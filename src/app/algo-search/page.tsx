621fd770e30c865125865fa637ddbf4d1ee4aa12
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-03-21 15:00:05 -0300

    Refactorizado codigo en algo search y algo sort

--- src/app/algo-search/page.tsx
+++ src/app/algo-search/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import NavigationTitle from "@/components/NavigationTitle";
 import React, { useEffect, useId } from "react";
+import info from "../../libs/Algo.json";
 
 const AlgoSearch = () => {
   const bloq = useId();
@@ -23,27 +24,16 @@ const AlgoSearch = () => {
           </h2>
         </div>
         <div className="pt-6 md:pt-8">
-          <div className="px-4 mb-10 md:px-12 lg:px-28 xl:px-44">
-            <p className="text-white text-xl underline mb-2 md:text-2xl">
-              Linear Search
-            </p>
-            <p className="text-white text-sm text-justify md:text-base">
-              Linear Search is defined as a sequential search algorithm that
-              starts at one end and goes through each element of a list until
-              the desired element is found, otherwise the search continues till
-              the end of the data set.
-            </p>
-          </div>
-          <div className="px-4 mb-10 md:px-12 lg:px-28 xl:px-44">
-            <p className="text-white text-xl underline mb-2 md:text-2xl">
-              Binary Search
-            </p>
-            <p className="text-white text-sm text-justify md:text-base">
-              Binary Search is defined as a searching algorithm used in a sorted
-              array by repeatedly dividing the search interval in half. To apply
-              Binary Search algorithm the data structure must be sorted.
-            </p>
-          </div>
+          {info.search.map((al) => (
+            <div className="px-4 mb-10 md:px-12 lg:px-28 xl:px-44">
+              <p className="text-white text-xl underline mb-2 md:text-2xl">
+                {al.title}
+              </p>
+              <p className="text-white text-sm text-justify md:text-base">
+                {al.info}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/app/algo-sort/page.tsx
+++ src/app/algo-sort/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import NavigationTitle from "@/components/NavigationTitle";
 import React, { useEffect, useId } from "react";
+import info from "../../libs/Algo.json";
 
 const AlgoSort = () => {
   const bloq = useId();
@@ -23,44 +24,16 @@ const AlgoSort = () => {
           </h2>
         </div>
         <div className="pt-6 md:pt-8">
-          <div className="px-4 mb-10 md:px-12 lg:px-28 xl:px-44">
-            <p className="text-white text-xl underline mb-2 md:text-2xl">
-              Bubble Sort
-            </p>
-            <p className="text-white text-sm text-justify md:text-base">
-              Bubble Sort is the simplest sorting algorithm that works by
-              repeatedly swapping the adjacent elements if they are in the wrong
-              order. This algorithm is not suitable for large data sets as its
-              average and worst-case time complexity is quite high.
-            </p>
-          </div>
-          <div className="px-4 mb-10 md:px-12 lg:px-28 xl:px-44">
-            <p className="text-white text-xl underline mb-2 md:text-2xl">
-              Gnome Sort
-            </p>
-            <p className="text-white text-sm text-justify md:text-base">
-              Gnome Sort also called Stupid sort is based on the concept of a
-              Garden Gnome sorting his flower pots. A garden gnome sorts the
-              flower pots by the following method- He looks at the flower pot
-              next to him and the previous one; if they are in the right order
-              he steps one pot forward, otherwise he swaps them and steps one
-              pot backwards. If there is no previous pot (he is at the starting
-              of the pot line), he steps forwards; if there is no pot next to
-              him (he is at the end of the pot line), he is done.
-            </p>
-          </div>
-          <div className="px-4 mb-10 md:px-12 lg:px-28 xl:px-44">
-            <p className="text-white text-xl underline mb-2 md:text-2xl">
-              Insertion Sort
-            </p>
-            <p className="text-white text-sm text-justify md:text-base">
-              Insertion sort is a simple sorting algorithm that works similarly
-              to the way you sort playing cards in your hands. The array is
-              virtually split into a sorted and an unsorted part. Values from
-              the unsorted part are picked and placed in the correct position in
-              the sorted part.
-            </p>
-          </div>
+          {info.sort.map((al) => (
+            <div className="px-4 mb-10 md:px-12 lg:px-28 xl:px-44">
+              <p className="text-white text-xl underline mb-2 md:text-2xl">
+                {al.title}
+              </p>
+              <p className="text-white text-sm text-justify md:text-base">
+                {al.info}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>

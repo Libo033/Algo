f6b5aa8c990dec09c60f59a558984ba74f6bd96b
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-03-24 21:19:32 -0300

    Añadido funcionalidad al componente AlgoSortPlayground

--- src/components/AlgoSortPlayground.tsx
+++ src/components/AlgoSortPlayground.tsx
@@ -4,26 +4,133 @@ import React, { useEffect, useState } from "react";
 
 const defArray = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-  61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-  80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
-  99,
+  23, 24, 25, 26, 27, 28, 29, 30,
 ];
 
 const AlgoSortPlayground = () => {
-  const [limit, setLimit] = useState(10);
   const [algo, setAlgo] = useState<string>("");
   const [time, setTime] = useState<number>(0);
   const [sortedValues, setSortedValues] = useState<number[]>(defArray);
+  const [reset, setReset] = useState<boolean>(false);
+  const [isSorting, setIsSorting] = useState<boolean>(false);
+
+  const handleStartSort = async () => {
+    try {
+      const delay = parseInt(
+        (document.getElementById("delay") as HTMLInputElement).value
+      );
+
+      if (algo === "B") {
+        // BUBBLE SORT
+        await bubbleSort(delay);
+      }
+      if (algo === "G") {
+        // GNOME SORT
+        await gnomeSort(delay);
+      }
+      if (algo === "I") {
+        // INSERTION SORT
+        await insertionSort(delay);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const bubbleSort = async (delay: number) => {
+    let begin = Date.now();
+
+    setIsSorting(true);
+    for (let i = 0; i < sortedValues.length; i++) {
+      for (let j = 0; j < sortedValues.length - i - 1; j++) {
+        if (sortedValues[j] > sortedValues[j + 1]) {
+          const temp = sortedValues[j];
+          sortedValues[j] = sortedValues[j + 1];
+          sortedValues[j + 1] = temp;
+          delay !== undefined ? await sleep(delay) : await sleep(0);
+          setSortedValues([...sortedValues]);
+        }
+      }
+    }
+    setIsSorting(false);
+    setReset(false);
+
+    let end = Date.now();
+    setTime((-begin + end) / 1000);
+  };
+
+  const gnomeSort = async (delay: number) => {
+    let begin = Date.now();
+
+    setIsSorting(true);
+    let pos = 1;
+
+    while (pos < sortedValues.length) {
+      if (pos == 0) pos++;
+
+      if (sortedValues[pos] >= sortedValues[pos - 1]) {
+        pos++;
+      } else {
+        let temp = 0;
+        temp = sortedValues[pos];
+        sortedValues[pos] = sortedValues[pos - 1];
+        sortedValues[pos - 1] = temp;
+        pos--;
+      }
+
+      delay !== undefined ? await sleep(delay) : await sleep(0);
+      setSortedValues([...sortedValues]);
+    }
+
+    setReset(false);
+    setIsSorting(false);
+
+    let end = Date.now();
+    setTime((-begin + end) / 1000);
+  };
+
+  const insertionSort = async (delay: number) => {
+    let begin = Date.now();
+
+    setIsSorting(true);
+    let n = sortedValues.length;
+
+    for (let i = 1; i < n; i++) {
+      let current = sortedValues[i];
+      let j = i - 1;
+      while (j > -1 && current < sortedValues[j]) {
+        sortedValues[j + 1] = sortedValues[j];
+        j--;
+      }
+      sortedValues[j + 1] = current;
+
+      delay !== undefined ? await sleep(delay) : await sleep(0);
+      setSortedValues([...sortedValues]);
+    }
+    setReset(false);
+    setIsSorting(false);
+
+    let end = Date.now();
+    setTime((-begin + end) / 1000);
+  };
+
+  const handleReset = (array: number[]) => {
+    setReset(true);
+    setSortedValues(array.sort(() => Math.random() - 0.5));
+    setTime(0);
+  };
 
   useEffect(() => {
     if (window) {
-      if (window.innerWidth <= 640) setLimit(10);
-      if (window.innerWidth > 640 && window.innerWidth <= 768) setLimit(16);
-      if (window.innerWidth > 768 && window.innerWidth <= 1024) setLimit(20);
-      if (window.innerWidth > 1024 && window.innerWidth <= 1280) setLimit(24);
-      if (window.innerWidth > 1280 && window.innerWidth <= 1536) setLimit(30);
+      if (window.innerWidth <= 640) setSortedValues(defArray.slice(0, 10));
+      if (window.innerWidth > 640 && window.innerWidth <= 768)
+        setSortedValues(defArray.slice(0, 16));
+      if (window.innerWidth > 768 && window.innerWidth <= 1024)
+        setSortedValues(defArray.slice(0, 20));
+      if (window.innerWidth > 1024 && window.innerWidth <= 1280)
+        setSortedValues(defArray.slice(0, 24));
+      if (window.innerWidth > 1280 && window.innerWidth <= 1536)
+        setSortedValues(defArray.slice(0, 30));
     }
   }, []);
 
@@ -31,8 +138,9 @@ const AlgoSortPlayground = () => {
     <div className="mx-4 mb-12 md:mx-12 lg:mx-28 xl:mx-44">
       <section className="w-full h-fit pt-6 bg-blue-100/20 border rounded-lg flex justify-evenly items-end">
         {sortedValues.length > 0 &&
-          sortedValues.slice(0, limit).map((v) => (
+          sortedValues.map((v) => (
             <span
+              key={v}
               style={{ height: `${20 + v * 15}px` }}
               className="bg-blue-100/90 pt-2 rounded-t-lg w-6 flex justify-center"
             >
@@ -41,14 +149,21 @@ const AlgoSortPlayground = () => {
           ))}
       </section>
       <div className="py-8 flex justify-center">
-        <button className="w-3/6 py-2 text-white border border-blue-300 rounded-md hover:bg-blue-50/10">
-          INICIAR
+        <button
+          onClick={() => {
+            reset ? handleStartSort() : handleReset(sortedValues);
+          }}
+          className="w-3/6 py-2 text-white border border-blue-300 rounded-md hover:bg-blue-50/10"
+          disabled={isSorting}
+        >
+          {reset ? "START" : "RESET"}
         </button>
       </div>
       <p className="text-white text-2xl">Options:</p>
       <div className="flex flex-col gap-3 mt-2 mb-4 md:flex-row lg:gap-6">
         <button
           onClick={() => setAlgo("B")}
+          disabled={isSorting}
           className={
             algo === "B"
               ? "w-full py-2 text-white border border-blue-300 rounded-md bg-blue-50/20"
@@ -59,6 +174,7 @@ const AlgoSortPlayground = () => {
         </button>
         <button
           onClick={() => setAlgo("G")}
+          disabled={isSorting}
           className={
             algo === "G"
               ? "w-full py-2 text-white border border-blue-300 rounded-md bg-blue-50/20"
@@ -69,6 +185,7 @@ const AlgoSortPlayground = () => {
         </button>
         <button
           onClick={() => setAlgo("I")}
+          disabled={isSorting}
           className={
             algo === "I"
               ? "w-full py-2 text-white border border-blue-300 rounded-md bg-blue-50/20"
@@ -87,6 +204,7 @@ const AlgoSortPlayground = () => {
           className="w-2/6 px-2 py-3 bg-slate-300 border border-blue-800 rounded-md outline-0 outline-blue-300 outline focus:bg-white focus:outline-1 hover:bg-slate-200 lg:w-1/6"
           type="number"
           autoComplete="off"
+          disabled={isSorting}
           defaultValue={10}
         />
       </div>
